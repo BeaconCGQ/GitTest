d2b681285cb7bada1186022614d77270133ecfc9
Beacon <2296695913@qq.com>
2021-07-20 21:16:32 +0800

    push commit

--- src/main/java/com/cgq/git/GitTest.java
+++ src/main/java/com/cgq/git/GitTest.java
@@ -7,9 +7,8 @@ public class GitTest {
         System.out.println("hello,Git2");
         System.out.println("hello,Git3");
         System.out.println("hello,Git4");
-
         System.out.println("master,test");
-
         System.out.println("hot-fix,Test");
+        System.out.println("push,Test");
     }
 }
